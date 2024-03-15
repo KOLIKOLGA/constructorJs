ced2d0d8474d369eec528dba6c2a7f3af0835f5e
OLGA KOLIKHALOVA <92117874+KOLIKOLGA@users.noreply.github.com>
2024-03-15 21:10:41 +0300

    add elem, text. delete divBlock, textBlock

--- script.js
+++ script.js
@@ -8,33 +8,27 @@ const DomElement = function (selector, height, width, bg, fontSize) {
   this.fontSize = fontSize;
 
   this.createElement = function () {
+    let elem;
+    let text;
     if (selector[0] === ".") {
-      const divBlock = document.createElement("div");
-      divBlock.innerHTML = ``;
-      divBlock.className = `${selector.slice(1)}`;
-      divBlock.style.cssText = `width:${width}; height:${height}; background:${bg}; font size:${fontSize}`;
-      console.log(divBlock);
-      document.body.appendChild(divBlock);
+      elem = document.createElement("div");
+      text = "div";
+      elem.className = `${selector.slice(1)}`;
     }
     if (selector[0] === "#") {
-      const textBlock = document.createElement("p");
-      textBlock.innerHTML = ``;
-      textBlock.id = `${selector.slice(1)}`;
-      textBlock.style.cssText = `width:${width}; height:${height}; background:${bg}; font size:${fontSize}`;
-      textBlock.textContent = `id = ${selector.slice(1)}`;
-      document.body.append(textBlock);
-      console.log(textBlock);
+      elem = document.createElement("p");
+      text = "paragraph";
+      elem.id = `${selector.slice(1)}`;
     }
+    elem.innerHTML = ``;
+    elem.style.cssText = `width:${width}; height:${height}; background:${bg}; font size:${fontSize}`;
+    elem.textContent = `${text} = ${selector.slice(1)}`;
+    console.log(elem);
+    document.body.append(elem);
   };
 };
 
 const newDomElement = new DomElement(".block", "100px", "100%", "red", "24px");
 newDomElement.createElement(".block");
-const newDomElement1 = new DomElement(
-  "#best",
-  "100px",
-  "100%",
-  "green",
-  "24px"
-);
+const newDomElement1 = new DomElement("#best", "50px", "100%", "green", "24px");
 newDomElement1.createElement("#best");
